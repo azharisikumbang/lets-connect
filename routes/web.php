5b25e522bdba19f17be1e422a41942235d21bdd7
Kum <azharisaputrasikumbang@gmail.com>
2023-11-05 01:01:53 +0700

    add breeze scafolding

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\ActivityManagementController;
-use App\Http\Controllers\CommunityConnectorController;
-use App\Http\Controllers\RegisterCommunityController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,11 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// public 
-Route::post('/mendaftar', [RegisterCommunityController::class, 'store']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/connect', [CommunityConnectorController::class, 'connect']);
-Route::post('/disconnect', [CommunityConnectorController::class, 'disconnect']);
-Route::resource('/activities', ActivityManagementController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/profil', function () { })->name('profil.index');
+require __DIR__.'/auth.php';
